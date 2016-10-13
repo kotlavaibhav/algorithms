a231f95cc97694b3da1c0349b0a19447cec467ce
kotlavaibhav <kotlavaibhav@gmail.com>
2016-10-12 18:08:44 -0700

    final

--- twosum.cpp
+++ twosum.cpp
@@ -16,23 +16,19 @@ vector<int> twoSum(vector<int>& nums, int target) {
 
 	
 }
+vector<int> twoSum(vector<int>& nums, int target) {
+	unordered_map<int, int> m1;
+	vector<int> x;
+	
 
-vector<int> twoSum(vector<int>& numbers, int target) {
-	unordered_map<int, int> hash;
-	vector<int> result;
-	for (int i = 0; i < numbers.size(); i++) {
-		int numberToFind = target - numbers[i];
+	for (int i = 0;i<nums.size();i++) {
+		if (m1.find(target - nums[i]) != m1.end()) {
+			x.push_back(m1[target - nums[i]]);
+			x.push_back(i+1);
 
-		//if numberToFind is found in map, return them
-		if (hash.find(numberToFind) != hash.end()) {
-			//+1 because indices are NOT zero based
-			result.push_back(hash[numberToFind] + 1);
-			result.push_back(i + 1);
-			return result;
+			return x;
 		}
-
-		//number was not found. Put it in the map.
-		hash[numbers[i]] = i;
+		m1[nums[i]] = i;
 	}
-	return result;
+	return x;
 }
